4afa5a2d21e01d282e929f8a4c93edaa57f77361
Sergio Maia <sergiomaia@gmail.com>
2017-06-06 03:12:40 -0300

    feat/create user resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170604200200) do
+ActiveRecord::Schema.define(version: 20170606052115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,10 +25,33 @@ ActiveRecord::Schema.define(version: 20170604200200) do
 
   create_table "estados", force: :cascade do |t|
     t.string   "sigla"
-    t.string   "nome",       null: false
+    t.string   "nome"
     t.integer  "capital_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["capital_id"], name: "index_estados_on_capital_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",               default: "",   null: false
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
+    t.text     "address"
+    t.string   "phone"
+    t.integer  "city_id",                               null: false
+    t.boolean  "active",                 default: true, null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,    null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
