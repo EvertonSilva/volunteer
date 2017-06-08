6c322c78e897a77999d0ba3173bd5f1e746bf093
Everton Silva <thonns@gmail.com>
2017-06-08 01:17:21 -0300

    feature/mockup for jobs board page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth' }
 	root to: "welcome#home"
   get 'welcome/home'
+
+  get 'jobs/board' => 'jobs#index'
 end
